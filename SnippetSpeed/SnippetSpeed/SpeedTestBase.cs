2cf7c255d267aaa60539288f482fbd5309147dbd
Sobieck00 <thomas@sobieck.com>
2016-10-08 11:09:48 -0500

    First Check in

---
+++ SnippetSpeed/SnippetSpeed/SpeedTestBase.cs
@@ -0,0 +1,9 @@
+﻿namespace SnippetSpeed
+{
+    public abstract class SpeedTestBase
+    {
+        public abstract void Act();
+
+        public abstract string TypeOfTest { get; }
+    }
+}
---
+++ SnippetSpeed/SnippetSpeed/TestableSpeedTestBase.cs
@@ -0,0 +1,12 @@
+﻿namespace SnippetSpeed
+{
+    public abstract class TestableSpeedTestBase<T> : SpeedTestBase where T : class
+    {
+        public abstract T TestableAct();
+
+        public override void Act()
+        {
+            TestableAct();
+        }
+    }
+}
